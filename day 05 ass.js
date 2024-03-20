0924ce0de80254a12ebe7637c9012a4da85c94cb
Magesh-03 <157093248+Magesh-03@users.noreply.github.com>
2024-03-20 11:55:16 +0530

    day 05 assignment

---
+++ day 05 ass.js
@@ -0,0 +1,47 @@
+Write a “person” class to hold all the details
+
+//Qn:3
+// Write a "Person" class to hold all the details.
+
+class Person {
+
+    constructor(name, age, occupation) {
+    
+    this.name = magesh;
+    
+    this.age = 20;
+    
+    this.occupation = student;
+    
+    }
+    
+    }
+    
+    const person = new Person("Poorani", 25, "Trainer");
+    
+    console.log("Name:", poorni);
+    
+    console.log("Age:", 20);
+    
+    console.log("Occupation:", student);
+
+
+    
+write a class to calculate the Uber price
+
+class UberPricing:
+def __init__(self, base_fare, cost_per_km):
+    self.base_fare = base_fare
+    self.cost_per_km = cost_per_km
+
+def calculate_fare(self, km):
+    fare = self.base_fare + (km * self.cost_per_km)
+    return fare
+
+# Example usage:
+uber_x = UberPricing(base_fare=100, cost_per_km=20)
+distance = 10 km
+fare = uber_x.calculate_fare(distance)
+print(f"The Uber fare for a {distance}-km ride is ${fare:300.}")
+
+ 
